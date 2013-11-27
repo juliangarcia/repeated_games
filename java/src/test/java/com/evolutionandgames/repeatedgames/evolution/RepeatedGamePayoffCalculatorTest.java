f81a2428b3f19d15c2b9faaeb310259c718aa5b6
Julian Garcia <julian.garcia@pobox.com>
2013-11-27 11:12:07 +0100

    Matching noise and no matching noise for payoff computation.

---
+++ java/src/test/java/com/evolutionandgames/repeatedgames/evolution/RepeatedGamePayoffCalculatorTest.java
@@ -0,0 +1,61 @@
+package com.evolutionandgames.repeatedgames.evolution;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.evolutionandgames.agentbased.Agent;
+import com.evolutionandgames.agentbased.AgentBasedPopulation;
+import com.evolutionandgames.agentbased.extensive.ExtensivePopulation;
+import com.evolutionandgames.agentbased.extensive.ExtensivePopulationImpl;
+import com.evolutionandgames.jevodyn.utils.Random;
+
+public class RepeatedGamePayoffCalculatorTest {
+
+	
+
+	@Before
+	public void setUp() throws Exception {
+		Random.seed();
+	}
+
+	@Test
+	public void testNoMatchingNoise() {
+		
+		RepeatedGame repeatedGame = new RepeatedGame(1.0, 1.0, 1.0, 1.0, 0.0);
+		RepeatedGamePayoffCalculator calculator = new RepeatedGamePayoffCalculator(repeatedGame, 0.0, false, false);
+		Agent[] agentArray = getDummyArray(Random.nextInt(50));
+		AgentBasedPopulation population = new ExtensivePopulationImpl(agentArray);
+		calculator.calculatePayoffs(population);
+		for (int i = 0; i < population.getSize(); i++) {
+			double payoff = ((ExtensivePopulation)population).getPayoffOfAgent(i);
+			assertEquals(population.getSize()-1.0, payoff, 0.0);
+		}
+	}
+	
+	
+	@Test
+	public void testWithMatchingNoise() {
+		
+		RepeatedGame repeatedGame = new RepeatedGame(1.0, 1.0, 1.0, 1.0, 0.0);
+		RepeatedGamePayoffCalculator calculator = new RepeatedGamePayoffCalculator(repeatedGame, 0.0, false, true);
+		Agent[] agentArray = getDummyArray(Random.nextInt(50));
+		AgentBasedPopulation population = new ExtensivePopulationImpl(agentArray);
+		calculator.calculatePayoffs(population);
+		for (int i = 0; i < population.getSize(); i++) {
+			double payoff = ((ExtensivePopulation)population).getPayoffOfAgent(i);
+			assertEquals(1.0, payoff, 0.0);
+		}
+	}
+	
+
+	private Agent[] getDummyArray(int size) {
+		Agent[] ans = new Agent[size];
+		for (int i = 0; i < ans.length; i++) {
+			ans[i] = new DummyAgent();
+		}
+		return ans;
+	}
+
+}
